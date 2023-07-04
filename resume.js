97cf22bbecd36a78b70d6512cec6d07a5b2f4df5
Giovanni Bwayo <134738410+GIOVESS@users.noreply.github.com>
2023-07-04 20:31:57 +0300

    Update resume.js

--- resume.js
+++ resume.js
@@ -1,1 +1,51 @@
+const body = document.querySelector("body");
+const container = document.querySelector(".container");
+const sidebar = document.querySelector(".sidebar");
+const content = document.querySelector(".content");
+
+// Set the margin and padding of the body element to 0
+body.style.margin = "0";
+body.style.padding = "0";
+
+// Set the font family of the body element to Arial
+body.style.fontFamily = "Arial, sans-serif";
+
+// Set the display property of the container element to flex
+container.style.display = "flex";
+
+// Set the width property of the sidebar element to 20%
+sidebar.style.width = "20%";
+
+// Set the background color of the sidebar element to black
+sidebar.style.backgroundColor = "#333";
+
+// Set the color of the sidebar element to white
+sidebar.style.color = "#fff";
+
+// Set the padding of the sidebar element to 20px
+sidebar.style.padding = "20px";
+
+// Set the width property of the content element to 80%
+content.style.width = "80%";
+
+// Set the padding of the content element to 20px
+content.style.padding = "20px";
+
+// Set the margin bottom property of the section element to 30px
+section.style.marginBottom = "30px";
+
+// Set the color of the headings to black
+h2.style.color = "#333";
+
+// Set the background color of the footer element to black
+footer.style.backgroundColor = "#333";
+
+// Set the color of the footer element to white
+footer.style.color = "#fff";
+
+// Set the padding of the footer element to 20px
+footer.style.padding = "20px";
+
+// Set the text align property of the footer element to center
+footer.style.textAlign = "center";
 
